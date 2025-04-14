439e506672b58052b78f9532e071d831533a3508
sagarmasal12 <sagarmasal12@gmail.com>
2025-04-14 07:03:11 +0530

    component changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './Pages/search/search.component';
+import { BookingComponent } from './Pages/booking/booking.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:"search",
+        pathMatch:"full"
+    },
+    {
+        path:"search",
+        component:SearchComponent
+    },
+    {
+        path:"booking",
+        component:BookingComponent
+    }
+];
